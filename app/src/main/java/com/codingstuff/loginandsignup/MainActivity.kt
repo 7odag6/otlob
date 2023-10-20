d3369fdeccb9d6dc6776080c7bca6d3f793843cd
7odag6 <ozo22superboy@gmail.com>
2023-10-20 20:22:46 +0300

    log

--- app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
+++ app/src/main/java/com/codingstuff/loginandsignup/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.codingstuff.loginandsignup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codingstuff.loginandsignup.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button2.setOnClickListener{
+            val intent = Intent (this,PersonalProfile::class.java)
+            startActivity(intent)
+        }
+        binding.button4.setOnClickListener{
+            val intent =Intent(this ,SignInActivity::class.java)
+            FirebaseAuth.getInstance().signOut();
+            startActivity(intent)
+        }
+
+
     }
 }
